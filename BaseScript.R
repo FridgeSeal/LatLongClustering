472bc2c023d89f6fb69de05122af61477574b966
FridgeSeal <toastedbananasandwhich@gmail.com>
2016-03-16 17:15:18 +1100

    Started data input.
    Using OpenData/Socrata URL rather than importing from downloaded file

--- BaseScript.R
+++ BaseScript.R
@@ -1,1 +1,3 @@
 # Basic R script to cluster/group some lat-longs about some predefined point and radius
+require(RSocrata) # For pulling the data straight off OpenData 
+data = read.socrata(url = "https://opendata.socrata.com/resource/txu4-fsic.json")
