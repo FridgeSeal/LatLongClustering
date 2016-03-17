47f758c9c936ccb58196b7f7f4d2583a552ee83a
FridgeSeal <toastedbananasandwhich@gmail.com>
2016-03-17 15:12:29 +1100

    Changed API call to only get the variables I actually need/want

--- BaseScript.R
+++ BaseScript.R
@@ -1,3 +1,4 @@
 # Basic R script to cluster/group some lat-longs about some predefined point and radius
-require(RSocrata) # For pulling the data straight off OpenData 
-data = read.socrata(url = "https://opendata.socrata.com/resource/txu4-fsic.csv")
+require(RSocrata) # For pulling the data straight off OpenData
+options(stringsAsFactors = FALSE)
+data = read.socrata(url = "https://opendata.socrata.com/resource/txu4-fsic.csv?$select=facility_id,latitude,longitude")
