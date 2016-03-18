ba847481a9aa73fd28501cdbc4a57abbdd3de82d
FridgeSeal <toastedbananasandwhich@gmail.com>
2016-03-18 17:05:31 +1100

    Added a plot without unclustered points

--- BaseScript.R
+++ BaseScript.R
@@ -6,11 +6,13 @@ require(ggmap)
 require(dbscan)
 options(stringsAsFactors = FALSE)
 GeoData = read.socrata(url = "https://opendata.socrata.com/resource/txu4-fsic.csv?$select=longitude,latitude")
-#GeoData = as.matrix(GeoData)
 CairoWin()
 MapBackground = get_map(location = c(-98.35,39.5), zoom = 4, scale = 2, maptype = "roadmap", source =  "google")
-ggmap(MapBackground) + geom_point(data = GeoData, aes(x = Longitude, y = Latitude, colour = Facility.ID), size = 0.01)
-GeoClust = dbscan(GeoData, eps = 1, minPts = 5)
+GeoClust = dbscan(GeoData, eps = 0.3, minPts = 4)
 GeoData$Cluster = matrix(unlist(GeoClust$cluster), ncol = 1, byrow = TRUE)
 View(GeoData)
-ggmap(MapBackground) + geom_point(data = testmatrix, aes(x = Longitude, y = Latitude, colour = Cluster), size = 0.01) +scale_colour_gradientn(colours = rainbow(40))
+ggmap(MapBackground) + geom_point(data = GeoData, aes(x = Longitude, y = Latitude, colour = Cluster), size = 0.01) +scale_colour_gradientn(colours = rainbow(40))
+# kNNdistplot() Figure out a better way to plot this so we can line up the values and actually get a readable result
+tempsubset = subset(GeoData, GeoData$Cluster > 0, select=c(Longitude, Latitude, Cluster)) # Subset to remove unclustered points
+0 %in% tempsubset$Cluster # Neat way to check if an element exists in a list obvs dont need to do this due to previous line but it's here as a note
+ggmap(MapBackground) + geom_point(data = tempsubset, aes(x = Longitude, y = Latitude, colour = Cluster), size = 0.001) +scale_colour_gradientn(colours = rainbow(40)) # Plot without unclustered points
