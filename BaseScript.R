dc23a523030a93dacd02676a9ec7a7591e2bf357
FridgeSeal <toastedbananasandwhich@gmail.com>
2016-03-23 17:54:06 +1100

    Added distance matrix using the Haversine method.

--- BaseScript.R
+++ BaseScript.R
@@ -17,3 +17,4 @@ ggmap(MapBackground) + geom_point(data = GeoData, aes(x = Longitude, y = Latitud
 tempsubset = subset(GeoData, GeoData$Cluster > 0, select=c(Longitude, Latitude, Cluster)) # Subset to remove unclustered points
 0 %in% tempsubset$Cluster # Neat way to check if an element exists in a list obvs dont need to do this due to previous line but it's here as a note
 ggmap(MapBackground) + geom_point(data = tempsubset, aes(x = Longitude, y = Latitude, colour = Cluster), size = 0.001) +scale_colour_gradientn(colours = rainbow(40)) # Plot without unclustered points
+dist.matrix = distm(GeoData, fun = distHaversine)
